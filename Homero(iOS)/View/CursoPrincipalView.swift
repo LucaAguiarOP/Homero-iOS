a9c6db5a5f2c63c8841a78476297ea5becd0fc83
DylanSkywalker <114362937+DylanSkywalker@users.noreply.github.com>
2023-06-15 21:14:57 -0300

    Navigation link em vez de button

---
+++ Homero(iOS)/View/CursoPrincipalView.swift
@@ -0,0 +1,20 @@
+//
+//  CursoPrincipalView.swift
+//  Homero(iOS)
+//
+//  Created by userext on 15/06/23.
+//
+
+import SwiftUI
+
+struct CursoPrincipalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CursoPrincipalView_Previews: PreviewProvider {
+    static var previews: some View {
+        CursoPrincipalView()
+    }
+}
