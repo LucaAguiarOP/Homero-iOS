27c18fbc7e34182547accf1febf75486b1a0b27d
DylanSkywalker <114362937+DylanSkywalker@users.noreply.github.com>
2023-06-16 21:38:02 -0300

    comeco tela principal

--- Homero(iOS)/View/CursoPrincipalView.swift
+++ Homero(iOS)/View/CursoPrincipalView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct CursoPrincipalView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20){
+            Text("Todos os cursos")
+                .padding(.trailing,137)
+                .foregroundColor(Color("Bluedark"))
+                .frame(width: .infinity)
+                .font(.title)
+                .lineLimit(1)
+                
+
+            Divider()
+                .overlay{
+                    LinearGradient(gradient: Gradient(colors: [Color("Bluedark"),.white]),startPoint: .top,endPoint: .bottom)
+                }
+                .padding(.bottom)
+                .frame(height: 4)
+            Textcurse(curso: "Estrutura de dados")
+            BarraViewEspecial(nomeimagem: "conteudo", text:"Conteúdo"){}
+            BarraViewEspecial(nomeimagem: "desempenho", text:"Meu desempenho"){}
+            BarraViewEspecial(nomeimagem: "howfunciona", text: "Como funciona"){}
+            BarraViewEspecial(nomeimagem: "sobrecurso", text: "Sobre o curso"){}
+            Spacer()
+        }
     }
 }
 
