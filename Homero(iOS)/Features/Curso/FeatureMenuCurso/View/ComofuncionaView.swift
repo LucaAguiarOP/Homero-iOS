7a807d7e3a46049649c29e360e66d22e713e24ff
DylanSkywalker <114362937+DylanSkywalker@users.noreply.github.com>
2023-06-20 21:17:16 -0300

    Tentando instanciar o projeto

---
+++ Homero(iOS)/Features/Curso/FeatureMenuCurso/View/ComofuncionaView.swift
@@ -0,0 +1,20 @@
+//
+//  ComofuncionaView.swift
+//  Homero(iOS)
+//
+//  Created by userext on 20/06/23.
+//
+
+import SwiftUI
+
+struct ComofuncionaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ComofuncionaView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComofuncionaView()
+    }
+}
---
+++ Homero(iOS)/Features/Curso/FeatureMenuCurso/View/IniciarcursoView.swift
@@ -0,0 +1,20 @@
+//
+//  IniciarcursoView.swift
+//  Homero(iOS)
+//
+//  Created by userext on 20/06/23.
+//
+
+import SwiftUI
+
+struct IniciarcursoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IniciarcursoView_Previews: PreviewProvider {
+    static var previews: some View {
+        IniciarcursoView()
+    }
+}
